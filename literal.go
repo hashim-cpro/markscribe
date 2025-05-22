c32e5c9f83470e96ea1af83c48ccd1269a0a089c
hashim-cpro <hashimcpro@gmail.com>
2025-05-22 22:15:40 +0500

    add my own forked markscribe as import

--- literal.go
+++ literal.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/muesli/markscribe/literal"
+import "github.com/hashim-cpro/markscribe/literal"
 
 func literalClubCurrentlyReading(count int) []literal.Book {
 	books, err := literal.CurrentlyReading()
